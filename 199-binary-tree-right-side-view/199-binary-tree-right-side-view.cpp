405b5847b98a9e58486637d70b5b6d24dc31a898
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-04-24 18:07:23 +0530

    Time: 6 ms (39.13%), Space: 11.9 MB (68.90%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -12,21 +12,16 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        // map to store horizontal level and Node
-        map<int, int> mp;
-        preOrder(root, 0, mp);
-        
         vector<int> res;
-        for(auto x:mp) {
-            res.push_back(x.second);
-        }
+        preOrder(root, 0, res);
         return res;
     }
-    void preOrder(TreeNode* root, int level, map<int, int>& mp) {
+    void preOrder(TreeNode* root, int level, vector<int>& res) {
         if(root != NULL) {
-            mp[level] =  root->val;
-            preOrder(root->left, level+1, mp);
-            preOrder(root->right, level+1, mp);
+            if(level == res.size()) res.push_back(root->val);
+            preOrder(root->right, level+1, res);
+            preOrder(root->left, level+1, res);
+            
         }
     }
 };
