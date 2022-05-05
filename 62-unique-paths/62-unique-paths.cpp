155d9d31b01f002d8d4f5ea685065b3c3d26b55d
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-05 15:43:25 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (5.16%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        // Tabulation
+        
         // we will start with m-1 and go till 0,0
         vector<vector<int>> dp(m, vector<int>(n, -1));
         return helper(m-1, n-1, dp);
     }
+    
     int helper(int i, int j, vector<vector<int>>& dp) {
         // Base Case
-        if(i == 0|| j==0) return 1; // Reached Destination
+        if(i == 0 && j==0) return 1; // Reached Destination
         if(i<0 || j<0) return 0; // Out of the boundary
         if(dp[i][j] != -1) return dp[i][j]; 
         // Do All the stuffs
