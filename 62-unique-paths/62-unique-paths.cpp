411aea78db7428804282d3c9a48009cd6355cc45
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-05 17:30:07 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (53.71%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    // Space Optimization
+    int uniquePaths(int m, int n) {
+        // take array to maintain one row
+        vector<int> prev(n, 0);
+        // Tabulation
+        for(int i=0;i<m;i++) {
+            vector<int> temp(n, 0); // to calculate current row
+            for(int j=0;j<n;++j) {
+                if(i == 0 || j == 0) {
+                     temp[j] = 1;
+                } else {
+                        temp[j] = prev[j] +  temp[j-1];            
+                }
+            }
+            prev = temp;
+        }
+        return prev[n-1];
+    }
+    /*
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, 0));
         // Tabulation
@@ -13,5 +32,5 @@ public:
             }
         }
         return dp[m-1][n-1];
-    }
+    }*/
 };
