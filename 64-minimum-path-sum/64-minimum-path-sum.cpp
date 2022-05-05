65989e9bf1b5d57d16aa0a83b1f25228c6bc99ed
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-05 18:42:37 +0530

    Time: 13 ms (47.85%), Space: 9.9 MB (57.23%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -4,20 +4,22 @@ public:
         if(grid.size()==0) return 0;
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));\
+        vector<int> prev(n, 0);
             
         for(int i=0;i<m;i++) {
+            vector<int> cur(n, 0);
             for(int j=0;j<n;j++) {
-                if(i == 0 && j==0) dp[i][j] =  grid[i][j];
+                if(i == 0 && j==0) cur[j] =  grid[i][j];
                 else {
                     int up = 1e9, left = 1e9;
-                    if(i>0) up = grid[i][j] + dp[i-1][j];
-                    if(j>0) left = grid[i][j] + dp[i][j-1];
-                    dp[i][j] = min(up, left);
+                    if(i>0) up = grid[i][j] + prev[j];
+                    if(j>0) left = grid[i][j] + cur[j-1];
+                    cur[j] = min(up, left);
                 }
             }
+            prev = cur;
         }
             
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
