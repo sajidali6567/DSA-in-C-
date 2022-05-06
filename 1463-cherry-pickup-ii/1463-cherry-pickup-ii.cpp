5a2efa86e737d7bab188f66c759782cbf08c3920
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-06 17:45:26 +0530

    Time: 123 ms (36.29%), Space: 14.3 MB (58.43%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int f(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
+        // Base case
+        if(j1 <0 || j2 < 0 || j1 >= grid[i].size() || j2 >= grid[i].size()) {
+            return -1e9;
+        }
+        // reaches the destination
+        if(i == grid.size()-1)  {
+            if(j1 == j2) {
+               return grid[i][j1];
+            } else {
+                return grid[i][j1] + grid[i][j2];
+            }
+        }
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxi = -1e9;
+        // Explore all the possibilities
+        // alice's path
+        for(int dj1=-1;dj1<=1;dj1++) {
+            for(int dj2=-1;dj2<=1;dj2++) {
+                if(j1 == j2) {
+                    maxi = max(maxi, grid[i][j1] + f(i+1, j1+dj1, j2+dj2, grid, dp));
+                } else {
+                    maxi = max(maxi, grid[i][j1] + grid[i][j2] + f(i+1, j1+dj1, j2+dj2, grid, dp));
+                }
+            }
+        }
+        return dp[i][j1][j2] = maxi;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        if(grid.size() == 0) return 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
+        return f(0, 0, grid[0].size()-1, grid, dp);
+    }
+};
