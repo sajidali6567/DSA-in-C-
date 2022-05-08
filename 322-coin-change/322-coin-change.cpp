0f658cba5f606fb3345f350d80d320ee23d8a208
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-08 09:55:26 +0530

    Time: 242 ms (12.12%), Space: 45 MB (21.56%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,8 +2,22 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
-        int res = f(coins.size()-1, amount, coins, dp);
-        return (res==1e9)?-1:res;
+        
+        for(int j=0;j<=amount;++j) {
+            if(j % coins[0] == 0) dp[0][j] = j/coins[0];
+            else dp[0][j] = 1e9;
+        }
+        for(int i=1;i<coins.size();++i) {
+            for(int j =0;j<= amount;++j) {
+                int notTake = dp[i-1][j];
+                int take = 1e9;
+                if(coins[i] <=j) {
+                    take = 1+dp[i][j-coins[i]];
+                }
+                dp[i][j] = min(take, notTake);
+            }
+        }
+        return (dp[coins.size()-1][amount] == 1e9)?-1:dp[coins.size()-1][amount];
     }
     
     int f(int index, int target, vector<int>& coins, vector<vector<int>>& dp) {
