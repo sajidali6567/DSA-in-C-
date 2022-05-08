bac3965219ee30c2a3105fcdac40ac153df936f8
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-08 09:48:36 +0530

    Time: 117 ms (51.40%), Space: 45.6 MB (17.54%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -2));
-    
-        int res = helper(coins, coins.size()-1, amount, dp);
-        if(res == 1e9) return -1;
-        else return res;
+        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
+        int res = f(coins.size()-1, amount, coins, dp);
+        return (res==1e9)?-1:res;
     }
-    // f(index, target) = minimum number of coins required to reach target using coins till index
-    int helper(vector<int>& coins, int index, int target, vector<vector<int>>& dp) {
+    
+    int f(int index, int target, vector<int>& coins, vector<vector<int>>& dp) {
+        // Base Case
         if(index == 0) {
-            if(target%coins[0] == 0) {
-                return target/coins[0];
-            } else {
-                return 1e9;
-            }
+            if(target % coins[0] == 0) return target/coins[0];
+            else return 1e9;
         }
-        if(dp[index][target] != -2) {
-            return dp[index][target];
-        }
-        int notTake = helper(coins, index-1, target, dp);
+        if(dp[index][target] != -1) return dp[index][target];
+        int notTake = f(index-1, target, coins, dp);
         int take = INT_MAX;
-        if(coins[index] <= target) {
-            take = 1+ helper(coins, index, target-coins[index], dp);
-        } 
+        if(coins[index] <=target) {
+            take = 1+f(index, target-coins[index], coins, dp);
+        }
         return dp[index][target] = min(take, notTake);
     }
 };
