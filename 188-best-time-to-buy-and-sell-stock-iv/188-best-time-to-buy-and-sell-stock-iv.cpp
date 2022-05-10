64e3d225dc2031806a777b259ebe295570cbdb59
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-10 18:30:50 +0530

    Time: 8 ms (80.96%), Space: 10.9 MB (82.12%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public: 
     int maxProfit(int cap, vector<int>& prices) {
-      vector<vector<int>> dp(prices.size()+1,vector<int>(2*cap+1, 0));
+      vector<int> front(2*cap+1, 0), cur(2*cap+1, 0);
         for(int index = prices.size()-1;index>=0;index--) {
             for(int trans = 0;trans<2*cap;trans++) {
                 int profit = 0;
         
                 // Allowed to Buy Cas
                 if(trans%2==0) {
-                    profit = max(-prices[index] + dp[index+1][trans+1],  dp[index+1][trans]);
+                    profit = max(-prices[index] + front[trans+1],  front[trans]);
                 } else {
                     // sell case
-                    profit = max(prices[index]+ dp[index+1][trans+1],  dp[index+1][trans]);
+                    profit = max(prices[index]+ front[trans+1],  front[trans]);
                 }
-                dp[index][trans] = profit;
+                cur[trans] = profit;
             }
+            front = cur;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
