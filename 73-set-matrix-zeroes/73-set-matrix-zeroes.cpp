47d4402d0c93ee5c9522c149ebfcb51f931898dc
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-11 19:45:10 +0530

    Time: 19 ms (54.68%), Space: 13.3 MB (54.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int row = matrix.size(); int col = matrix[0].size();
-        int isSetByZeroThCol = 1;                                      
+        bool isSetByZeroThCol = false;
+        // we are changing a[0][0] only if any 0th row element is zero
+        // for 0th col we taking a variable which will determine if any entry in 0th col is zero or not
         for(int i=0;i<row;i++) {
             // if any of the element in 0th row is set to zero, mark the variable as 0
-            if(matrix[i][0] == 0) isSetByZeroThCol = 0;
+            if(matrix[i][0] == 0) isSetByZeroThCol = true;
             for(int j=1;j<col;++j) {
                 if(matrix[i][j]==0) {
                     matrix[i][0] = matrix[0][j] = 0;
@@ -19,7 +21,7 @@ public:
                     matrix[i][j] = 0;
                 }
             }
-            if(isSetByZeroThCol == 0) matrix[i][0] = 0;
+            if(isSetByZeroThCol) matrix[i][0] = 0;
         }
     }
 };
