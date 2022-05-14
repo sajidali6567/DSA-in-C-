048593a266f11ae7f4342861d262b1f7d2666b46
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-14 10:05:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (72.43%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+    int f(string& s, int n, vector<int>& dp) {
+        if(n == 0 || n ==1) {
+            return 1;
+        }
+        if(s[0] == '0') return 0;
+        
+        if(dp[n] != -1) return dp[n];
+        int count = 0;
+        if(s[n-1] > '0') {
+            count = f(s, n-1, dp);
+        }
+        if(s[n-2] == '1' || (s[n-2] == '2' && s[n-1] < '7') ) {
+            count += f(s, n-2, dp);
+        }
+        return dp[n] = count;
+    }
     int numDecodings(string s) {
         vector<int> dp(s.length()+1, -1);
-        return decodeWays(s, s.length(), dp);
-    }
-    int decodeWays(string s, int k,vector<int>& dp) {
-        if(k == 0 ) return 1;
-        int st = s.length()-k;
-        if(s[st] == '0') return 0;
-        if(dp[k] != -1) {
-            return dp[k];
-        }
-        // recursive call
-        int result = decodeWays(s, k-1, dp);
-        if(k>=2 && s.substr(st, 2) >= "10" && s.substr(st, 2) <= "26") {
-            result += decodeWays(s, k-2, dp);
-        }
-        return dp[k] = result;
+        if (s.length() == 0 || (s.length() == 1 && s[0] == '0'))
+            return 0;
+        return f(s, s.length(), dp);
     }
 };
