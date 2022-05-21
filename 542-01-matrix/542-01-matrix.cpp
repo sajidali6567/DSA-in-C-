07fd0055787d8cb36959e725de00efe45d1ab23e
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-21 21:11:34 +0530

    Time: 94 ms (62.66%), Space: 32.2 MB (22.33%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -3,15 +3,15 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size(); int n = mat[0].size();
         
-        vector<vector<int>> output(m, vector<int>(n, INT_MAX));
+        vector<vector<int>> output(m, vector<int>(n, 0));
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        queue<pair<int, pair<int, int>>> q;
+        queue<pair<int, int>> q;
         
         for(int i=0;i<mat.size();++i) {
             for(int j=0;j<mat[0].size();j++) {
                 if(mat[i][j]== 0) {
-                    output[i][j] = 0;
-                    q.push({i, {j, 0}});
+                    q.push({i, j});
+                    visited[i][j] = true;
                 }   
             }
         }
@@ -23,14 +23,14 @@ public:
             int cnt = q.size();
             while(cnt--) {
                 int i = q.front().first;
-                int j = q.front().second.first;
-                int level = q.front().second.second; q.pop();
+                int j = q.front().second;
+                q.pop();
                 for(int k=0;k<4;k++) {
                     int newI = i + dx[k];
                     int newJ = j + dy[k];
                     if(newI <0 || newJ <0 || newI >=m || newJ >= n || visited[newI][newJ]) continue;
-                    q.push({newI, {newJ, level+1}});
-                    output[newI][newJ] = min(output[newI][newJ], level+1);
+                    q.push({newI, newJ});
+                    output[newI][newJ] = output[i][j] + 1;
                     visited[newI][newJ] = true;
                 }
             } 
