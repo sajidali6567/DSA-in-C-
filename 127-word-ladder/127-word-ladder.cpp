a6ee33fb0ef960287d759104282bce89baad778d
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-21 12:59:31 +0530

    Time: 141 ms (69.95%), Space: 13.8 MB (89.55%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -4,18 +4,16 @@ public:
         // To store all the words in a dictionary, will facilitate to search a word in O(1)
         unordered_set<string> dict(wordList.begin(), wordList.end());
         // if endWord is not present in the dictionary return 0
-        cout << "cp1\n";
         if(dict.find(endWord) == dict.end()) return 0;
-        cout << "cp2\n";
         queue<string> q;
         q.push(beginWord);
         int depth = 1;
+        dict.erase(beginWord);
         // level order traversal line by to to get the depth of endWord
         while(!q.empty()) {
             int count = q.size();
             while(count--) {
                 string cur = q.front(); q.pop();
-                cout << cur << endl;
                 // traverse all the character one by one and replace it with all possible
                 // charcaters except the one it is
                 for(int i=0;i<cur.length();i++) {
@@ -24,7 +22,6 @@ public:
                         temp[i] = x;
                         // check it temp is endWord
                         if(temp == endWord) {
-                            cout << "returning from here" << endl;
                             return depth+1;
                         }
                         //  check if temp is present in wordList
