bc52a7378a9f62c33b7e4bf17fefb663d04ac1b1
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-05-27 13:21:06 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (84.73%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        vector<int>::iterator it = lower_bound(nums.begin(), nums.end(), target);
-        if(it == nums.end()) return {};
+        
+        int start = 0, end = nums.size()-1;
+        while(start < end) {
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target) end = mid;
+            else if (target > nums[mid]) start=mid+1;
+            else end=mid-1;
+        }
+        // cout << "end = " << end << endl;
+        if(end == -1) return {};
+        int lower = nums[end]==target?end:-1;
+        // cout << "lower = " << lower << endl;
+        if(lower == -1) return {};
         
         vector<int> res;
-        while(it != nums.end() && *it == target) {
-            res.push_back(it-nums.begin());
-            it++;
+        while(lower < nums.size() && nums[lower] == target) {
+            res.push_back(lower);
+            lower++;
         }
         return res;
     }
