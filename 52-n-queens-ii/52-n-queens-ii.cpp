c486c459629ac293e56564e2e12688cca48d86e0
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-05 00:16:48 +0530

    Time: 6 ms (64.24%), Space: 6.1 MB (62.33%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        int res = 0;
+        vector<string> grid(n, string(n, '.'));
+        solve(0, grid, n, res);
+        return res;
+    }
+    void solve(int col, vector<string>& grid, int n, int& res) {
+        if(col == n) { res++; return; }
+        for(int row=0;row<n;row++) {
+            if(isSafeToPlaceQueen(grid, row, col, n)) {
+                    grid[row][col] = 'Q';
+                    solve(col+1, grid, n, res);
+                    grid[row][col] = '.';
+            }
+        }
+    }
+    bool isSafeToPlaceQueen(vector<string>& grid, int row, int col, int n) {
+        // check row
+        for(int j=0;j<col;j++) {
+            if(grid[row][j] == 'Q') return false;
+        }
+        // check upper diagonal
+        int i = row - 1, j = col-1;
+        while(i>=0 && j >=0) {
+            if(grid[i][j] == 'Q') return false;
+            i--;j--;
+        }
+        // check lower diagonal
+        i = row + 1, j = col -1;
+        while(i < n && j >=0) {
+            if(grid[i][j] == 'Q') return false;
+            i++;j--;
+        }
+        return true;
+    }
+};
