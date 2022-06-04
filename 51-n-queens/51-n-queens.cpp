1a8a24d8e01b51b13ef7863cd08688327e0bdbe1
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-05 00:14:06 +0530

    Time: 13 ms (43.11%), Space: 7.2 MB (86.66%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -2,42 +2,35 @@ class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> res;
-        vector<vector<bool>> grid(n, vector<bool>(n, false));
-        int count = 0;
-        vector<string> temp;
-        solve(0, res, grid, n, count, temp);
-        cout << "count = " <<  count << endl;
+        vector<string> grid(n, string(n, '.'));
+        solve(0, grid, n, res);
         return res;
     }
-    void solve(int col, vector<vector<string>>& res, vector<vector<bool>>& grid, int n, int& count, vector<string> temp) {
-        if(col == n) { count++; res.push_back(temp); return;}
-        string str(n, '.');
+    void solve(int col, vector<string>& grid, int n, vector<vector<string>>& res) {
+        if(col == n) { res.push_back(grid); return; }
         for(int row=0;row<n;row++) {
             if(isSafeToPlaceQueen(grid, row, col, n)) {
-                    grid[row][col] = true;
-                    str[row] = 'Q'; temp.push_back(str);
-                    solve(col+1, res, grid, n, count, temp);
-                    grid[row][col] = false;
-                    str[row] = '.';temp.pop_back();
+                    grid[row][col] = 'Q';
+                    solve(col+1, grid, n, res);
+                    grid[row][col] = '.';
             }
         }
     }
-    bool isSafeToPlaceQueen(vector<vector<bool>>& grid, int row, int col, int n) {
-        if(col == 0) return true;
+    bool isSafeToPlaceQueen(vector<string>& grid, int row, int col, int n) {
         // check row
         for(int j=0;j<col;j++) {
-            if(grid[row][j]) return false;
+            if(grid[row][j] == 'Q') return false;
         }
         // check upper diagonal
         int i = row - 1, j = col-1;
         while(i>=0 && j >=0) {
-            if(grid[i][j]) return false;
+            if(grid[i][j] == 'Q') return false;
             i--;j--;
         }
         // check lower diagonal
         i = row + 1, j = col -1;
         while(i < n && j >=0) {
-            if(grid[i][j]) return false;
+            if(grid[i][j] == 'Q') return false;
             i++;j--;
         }
         return true;
