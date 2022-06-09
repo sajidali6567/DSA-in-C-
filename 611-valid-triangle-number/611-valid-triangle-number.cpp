dc2de13ff3400cdad66d9bbf97cf4bb3cd7fc64f
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-09 23:08:08 +0530

    Time: 323 ms (34.19%), Space: 13 MB (5.89%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    // check if three value form a triangle or not
-    bool isValidTriangle(int a, int b, int c) {
-        return (a+b>c)&&(a+c>b)&&(b+c>a);
-    }
     int triangleNumber(vector<int>& nums) {
-        if(nums.size() < 3) return 0;
         sort(nums.begin(), nums.end());
         int count = 0;
         for(int i=0;i<nums.size();i++) {
+            int k = i+2;
             for(int j=i+1;j<nums.size();j++) {
-                // search for all such c for which a + b > c
-                auto it = upper_bound(nums.begin(), nums.end(), nums[i]+nums[j]-1);
-                int temp = (it-nums.begin()-j-1);
-                if(temp > 0) count += temp;
+                while(k < nums.size() && nums[k]<nums[i]+nums[j]) {
+                    k++;
+                }
+                if(k-j-1 > 0) count += k-j-1;
             }
         }
         return count;
