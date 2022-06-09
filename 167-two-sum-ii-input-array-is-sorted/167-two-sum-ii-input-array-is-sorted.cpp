217b83e7fa7b590a473910ed5762fa5e79b38fcf
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-09 12:47:19 +0530

    Time: 23 ms (30.54%), Space: 15.6 MB (75.27%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    // Time Complexity O(nlogn)
+    // Space Complexity O(1)
     vector<int> twoSum(vector<int>& numbers, int target) {
         for(int i=0;i<numbers.size();i++) {
             int idx = -1;
-            // check if target-numbers[i] exists in numbers
+            /* check if target-numbers[i] exists in numbers, (equal sign is imp)                                           *
+             * because if target-numbers[i] is equal to numbers[i] and if it exists it will always exist on the right side */
             if(target-numbers[i] >= numbers[i]) { // check in right side
                 idx = binary_search(numbers, i+1, numbers.size()-1, target-numbers[i]);
                 if(idx != -1) {
                     return {i+1, idx+1};
                 }
-            } else {
+            } else { // check left side
                 idx = binary_search(numbers, 0, i+1, target-numbers[i]);
                 if(idx != -1) {
                     return {idx+1, i+1};
@@ -30,7 +33,6 @@ public:
                 high = mid - 1;
             }
         }
-        // it should never come here as we know solution always exists
         return -1;
     }
 };
