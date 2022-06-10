ad803f22f96a22f71e932e2ff5bcf980fd49765e
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-10 23:20:28 +0530

    Time: 108 ms (43.52%), Space: 25.5 MB (84.68%) - LeetHub

---
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    #define pp pair<int,int>
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        vector<pair<int, int>> arr;
+        for(int i=0;i<intervals.size();i++) {
+            // {start, index}
+            arr.push_back({intervals[i][0], i});
+        }
+        // sort the arr vector
+        sort(arr.begin(), arr.end());
+        
+        // traverse the array and find upper_bound for all the ends, if it exists push index in res or -1
+        vector<int> res;
+        for(int i=0;i<intervals.size();i++) {
+            int end = intervals[i][1];
+            auto it = lower_bound(arr.begin(), arr.end(), pp(end, INT_MIN), [&](pair<int,int> e1, pair<int,int> e2) {
+                if(e1.first!=e2.first)
+                    return e1.first<e2.first;
+                else
+                    return e1.second<e2.second;
+            });
+            if(it == arr.end()) {
+                res.push_back(-1);
+            } else {
+                res.push_back( arr[it-arr.begin()].second );
+            }
+        }
+        return res;
+    }
+};
