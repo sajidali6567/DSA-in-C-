7dc40f622148e069ef975ad926682006b10eb0dd
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-16 00:33:43 +0530

    Time: 1285 ms (5.64%), Space: 292.3 MB (6.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -15,7 +15,18 @@ public:
         return dp[idx][prevIdx+1] = max(pick, notPick);
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1, -1));
-        return solve(nums, 0, -1, dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int idx=n-1;idx>=0;idx--) {
+            for(int prevIdx=idx-1;prevIdx>=-1;prevIdx--) {
+                int notPick = 0 + dp[idx+1][prevIdx+1];
+                int pick = -1;
+                if(prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+                    pick = 1 + dp[idx+1][idx+1];
+                }
+                dp[idx][prevIdx+1] = max(pick, notPick);
+            }
+        }
+        return dp[0][-1+1];
     }
 };
