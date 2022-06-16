108c7094aef2ee0ecaa5d85d088290437f6c2afb
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-16 23:44:06 +0530

    Time: 445 ms (28.20%), Space: 231.7 MB (16.94%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     string longestPalindrome(string word1) {
-        string word2(word1);
-        reverse(word2.begin(), word2.end());
-        int ans = 0, index = -1;
-        // find longest common substring of word1 and word2
-        int M = word1.size(), N = word2.size();
-        vector<vector<int>> dp(M+1, vector<int>(N+1, 0));
-        for(int i=1;i<=M;i++) {
-            for(int j=1;j<=N;j++) {
-                if(word1[i-1] == word2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    if(dp[i][j] > ans ) {
-                        string temp1 = word1.substr(i-dp[i][j], dp[i][j]);
-                        string temp2 = word2.substr(N-i, dp[i][j]);
-                        if(temp1 == temp2) {
-                            ans = max(ans, dp[i][j]);
-                            index = i;                            
-                        }
-                    }                   
-                }
+        int n = word1.size();
+        int start = 0, end =0;
+        for(int i=0;i<n;i++) {
+            int a = expandAround(word1, i, i);
+            int b = expandAround(word1, i, i+1);
+            int len = max(a, b);
+            cout << len << " ";
+            if(len > end-start) {
+                start = i - (len-1)/2;
+                end =  i + len/2;
             }
         }
-        string res = "";
-        if(index != -1) res = word1.substr(index-ans, ans);            
-        return res;
+        return word1.substr(start, end-start+1);
+    }
+    int expandAround(string word, int L, int R) {
+        while(L>=0 && R < word.size() && word[L] == word[R]) {
+            L--;R++;
+        }
+        return R-L-1;
     }
 };
