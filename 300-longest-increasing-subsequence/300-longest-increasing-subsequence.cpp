b0a0ffba441afdd8eaca560c8fcad7142cce3eee
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-16 13:07:01 +0530

    Time: 853 ms (9.40%), Space: 292.2 MB (6.32%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    // f(idx, prev) = length of LIS starting from index idx whose previous element
-    // is at prev index
-    int solve(vector<int>& nums, int idx, int prevIdx, vector<vector<int>>& dp) {
-        if(idx == nums.size()) {
-            return 0;
-        }
-        if (dp[idx][prevIdx+1] != -1) return dp[idx][prevIdx+1];
-        int notPick = 0 + solve(nums, idx+1, prevIdx, dp);
-        int pick = -1;
-        if(prevIdx == -1 || nums[idx] > nums[prevIdx]) {
-            pick = 1 + solve(nums, idx+1, idx, dp);
-        }
-        return dp[idx][prevIdx+1] = max(pick, notPick);
-    }
+    // we need to keep prevIdx to check if next element can be included in our subsequence or not
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        for(int idx=n-1;idx>=0;idx--) {
-            for(int prevIdx=idx-1;prevIdx>=-1;prevIdx--) {
-                int notPick = 0 + dp[idx+1][prevIdx+1];
-                int pick = -1;
+        for(int idx = n-1;idx>=0;idx--) {
+            for(int prevIdx=idx;prevIdx>=-1; prevIdx--) {
+                int len = 0 + dp[idx+1][prevIdx+1];
                 if(prevIdx == -1 || nums[idx] > nums[prevIdx]) {
-                    pick = 1 + dp[idx+1][idx+1];
+                    len = max(len, 1 + dp[idx+1][idx+1]);
                 }
-                dp[idx][prevIdx+1] = max(pick, notPick);
+                dp[idx][prevIdx+1] = len;
             }
         }
         return dp[0][-1+1];
