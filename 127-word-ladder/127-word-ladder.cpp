0f6fc815d8c90a027b4d1214892cc32cd656399b
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-17 21:22:49 +0530

    Time: 96 ms (90.44%), Space: 13.8 MB (88.09%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
+    void print(queue<string> q) {
+        while(!q.empty()) {
+            cout << q.front() << " ";
+            q.pop();
+        }
+        cout << endl;
+    }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        // To store all the words in a dictionary, will facilitate to search a word in O(1)
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        // if endWord is not present in the dictionary return 0
+        unordered_set dict(wordList.begin(), wordList.end());
         if(dict.find(endWord) == dict.end()) return 0;
         queue<string> q;
         q.push(beginWord);
-        int depth = 1;
-        dict.erase(beginWord);
-        // level order traversal line by to to get the depth of endWord
+        int level = 0;
         while(!q.empty()) {
+            level += 1;
             int count = q.size();
+            // print(q);
             while(count--) {
-                string cur = q.front(); q.pop();
-                // traverse all the character one by one and replace it with all possible
-                // charcaters except the one it is
-                for(int i=0;i<cur.length();i++) {
-                    string temp = cur;
-                    for(char x = 'a'; x<='z';x++) {
-                        temp[i] = x;
-                        // check it temp is endWord
-                        if(temp == endWord) {
-                            return depth+1;
-                        }
-                        //  check if temp is present in wordList
+                string temp = q.front();q.pop();
+                // traverse all adjecent nodes of temp (if they exist in the dict)
+                for(int i=0;i<temp.size();i++) {
+                    char orig = temp[i];
+                    for(char ch='a';ch<='z';ch++) {
+                        if(ch ==orig) continue;
+                        temp[i] = ch;
+                        // if temp is present in dict push into the queue
                         if(dict.find(temp) != dict.end()) {
+                            if(temp == endWord) return level+1;
                             q.push(temp);
                             dict.erase(temp);
                         }
                     }
+                    temp[i] = orig;
                 }
             }
-            depth+=1;
         }
         return 0;
     }
