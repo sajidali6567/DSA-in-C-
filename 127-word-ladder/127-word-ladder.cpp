27fd3d04f4b7d979f090c9c7100706df8afd26a7
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-17 21:26:50 +0530

    Time: 137 ms (75.51%), Space: 13.8 MB (81.58%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    void print(queue<string> q) {
-        while(!q.empty()) {
-            cout << q.front() << " ";
-            q.pop();
-        }
-        cout << endl;
-    }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set dict(wordList.begin(), wordList.end());
         if(dict.find(endWord) == dict.end()) return 0;
@@ -16,14 +9,12 @@ public:
         while(!q.empty()) {
             level += 1;
             int count = q.size();
-            // print(q);
             while(count--) {
                 string temp = q.front();q.pop();
                 // traverse all adjecent nodes of temp (if they exist in the dict)
                 for(int i=0;i<temp.size();i++) {
                     char orig = temp[i];
                     for(char ch='a';ch<='z';ch++) {
-                        if(ch ==orig) continue;
                         temp[i] = ch;
                         // if temp is present in dict push into the queue
                         if(dict.find(temp) != dict.end()) {
