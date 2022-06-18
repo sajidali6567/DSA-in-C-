58e8c1a4767abc96cf64329c38ed3db8a0df8935
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-18 20:06:29 +0530

    Time: 9 ms (57.91%), Space: 9.1 MB (7.03%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res(nums1.size(), -1);
-        for(int i=0;i<nums1.size();i++) {
-            // check next greater element of nums1[i]
-            bool isFound = false;
-            for(int j=0;j<nums2.size();j++) {
-                if(nums2[j] == nums1[i]) {
-                    isFound = true;
-                } else if(isFound and nums2[j]>nums1[i]) {
-                    res[i] = nums2[j];
-                    break;
-                }
+        map<int, int> nge;
+        stack<int> st;
+        for(int i=0;i<nums2.size();i++) {
+            while(!st.empty() and nums2[st.top()] < nums2[i]) {
+                nge[nums2[st.top()]] = nums2[i];
+                st.pop();
             }
+            st.push(i);
+        }
+        while(!st.empty()) {
+            nge[nums2[st.top()]] = -1;
+            st.pop();
+        }
+        vector<int> res;
+        for(int i=0;i<nums1.size();i++) {
+            res.push_back(nge[nums1[i]]);
         }
         return res;
     }
