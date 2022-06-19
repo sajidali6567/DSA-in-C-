469e13becd68778573b271bef2a56ea2d00f7d04
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-19 12:13:48 +0530

    Time: 664 ms (48.73%), Space: 142.4 MB (99.56%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -4,20 +4,16 @@ public:
     NumMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> temp(m+1, vector<int>(n+1, 0));
-        psm = temp;
+        psm.resize(m+1, vector<int>(n+1)); // psm is 1-based indexing
         for(int i=1;i<=m;i++) {
             for(int j=1;j<=n;j++) {
-                psm[i][j] = psm[i-1][j] + psm[i][j-1] + matrix[i-1][j-1] - psm[i-1][j-1];
-            }
+                psm[i][j] = matrix[i-1][j-1] + psm[i-1][j] + psm[i][j-1] - psm[i-1][j-1];
+            } 
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        // General formula to obtain the sum of a rectangle using two co-ord
-        // psm[row2][col2] - psm[row1-1][col2]  - psm[row2][col1-1]  + psm[row1-1][col1-1]
-        // Adding +1 in each index as we took psm as 1 based indexing
-        return psm[row2+1][col2+1] - psm[row1][col2+1] - psm[row2+1][col1] + psm[row1][col1];
+        return psm[row2+1][col2+1] - psm[row2+1][col1] - psm[row1][col2+1] + psm[row1][col1];
     }
 };
 
