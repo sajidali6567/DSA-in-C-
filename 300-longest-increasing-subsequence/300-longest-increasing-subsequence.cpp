d96e8676f16e655bc59c9d277175a023918a7b0e
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-06-25 17:46:20 +0530

    Time: 321 ms (56.67%), Space: 10.5 MB (66.37%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    // we need to keep prevIdx to check if next element can be included in our subsequence or not
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> next(n+1, 0); vector<int> cur(n+1, 0);
-        for(int idx = n-1;idx>=0;idx--) {
-            for(int prevIdx=idx;prevIdx>=-1; prevIdx--) {
-                int len = 0 + next[prevIdx+1];
-                if(prevIdx == -1 || nums[idx] > nums[prevIdx]) {
-                    len = max(len, 1 + next[idx+1]);
+        vector<int> lis(n+1, 1);
+        int ans = 1;
+        for(int i = 0;i<n;i++) {
+            int maxi = 1;
+            for(int prev = 0; prev<i;prev++) {
+                if(nums[i] > nums[prev]) {
+                    maxi = max(maxi, 1+lis[prev]);
                 }
-                cur[prevIdx+1] = len;
             }
-            next = cur;
+            lis[i] = maxi;
+            ans = max(ans, maxi);
         }
-        return cur[-1+1];
+        return ans;
     }
 };
