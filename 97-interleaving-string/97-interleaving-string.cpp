29885508f33b78636e07bf2df907a3648077c09f
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-07-08 01:00:17 +0530

    Time: 2 ms (87.90%), Space: 6.1 MB (96.49%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int dp[101][101][201];
-    bool solve(string& s1, string& s2, string& s3, int i, int j, int k) {
-        if(i== s1.size() && j == s2.size() && k == s3.size()) {
+    int dp[101][101];
+    bool solve(string& s1, string& s2, string& s3, int i, int j) {
+        if(i== s1.size() && j == s2.size() && i+j == s3.size()) {
             return true;
         }
-        if(dp[i][j][k] != -1) return dp[i][j][k];
+        if(dp[i][j] != -1) return dp[i][j];
         int b = false;
-        if(i < s1.size() && s3[k] == s1[i]) {
-            b = solve(s1, s2, s3, i+1, j, k+1);
+        if(i < s1.size() && s3[i+j] == s1[i]) {
+            b = solve(s1, s2, s3, i+1, j);
         }
-        if(j < s2.size() && s3[k] == s2[j]) {
-            b = b || solve(s1, s2, s3, i, j+1, k+1);
+        if(j < s2.size() && s3[i+j] == s2[j]) {
+            b = b || solve(s1, s2, s3, i, j+1);
         }
-        return dp[i][j][k] = b;
+        return dp[i][j] = b;
     }
     bool isInterleave(string s1, string s2, string s3) {
         if(s3.size() > s1.size() + s2.size()) return false;
         memset(dp, -1, sizeof(dp));
-        return solve(s1, s2, s3, 0, 0, 0);
+        return solve(s1, s2, s3, 0, 0);
     }
 };
