242aa38870238aed086a62a716b1bd1a66305a92
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-07-08 01:12:31 +0530

    Time: 355 ms (5.00%), Space: 15.1 MB (34.24%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int dp[101][101];
-    bool solve(string& s1, string& s2, string& s3, int i, int j) {
-        if(i== s1.size() && j == s2.size() && i+j == s3.size()) {
+    int dp[101][101][201];
+    bool solve(string& s1, string& s2, string& s3, int i, int j, int k) {
+        if(i== s1.size() && j == s2.size() && k == s3.size()) {
             return true;
         }
-        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j][k] != -1) return dp[i][j][k];
         int b = false;
-        if(i < s1.size() && s3[i+j] == s1[i]) {
-            b = solve(s1, s2, s3, i+1, j);
+        if(i < s1.size() && s3[k] == s1[i]) {
+            b = solve(s1, s2, s3, i+1, j, k+1);
         }
-        if(j < s2.size() && s3[i+j] == s2[j]) {
-            b = b || solve(s1, s2, s3, i, j+1);
+        if(j < s2.size() && s3[k] == s2[j]) {
+            b = b || solve(s1, s2, s3, i, j+1, k+1);
         }
-        return dp[i][j] = b;
+        return dp[i][j][k] = b;
     }
     bool isInterleave(string s1, string s2, string s3) {
         if(s3.size() > s1.size() + s2.size()) return false;
-        memset(dp, -1, sizeof(dp));
-        return solve(s1, s2, s3, 0, 0);
+        memset(dp, 0, sizeof(dp));
+        for(int i=s1.size();i>=0;i--) {
+            for(int j=s2.size();j>=0;j--) {
+                for(int k = s3.size();k>=0;k--) {
+                    if(i== s1.size() && j == s2.size() && k == s3.size()) {
+                        dp[i][j][k] = true;
+                        continue;
+                    }
+                    int b = false;
+                    if(i < s1.size() && s3[k] == s1[i]) {
+                        b = dp[i+1][j][k+1];
+                    }
+                    if(j < s2.size() && s3[k] == s2[j]) {
+                        b = b || dp[i][j+1][k+1];
+                    }
+                    dp[i][j][k] = b;
+                }
+            }
+        }
+        return dp[0][0][0];
     }
 };
