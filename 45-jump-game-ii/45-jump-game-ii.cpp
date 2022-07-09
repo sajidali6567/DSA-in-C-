fb6221b21b3cfbd30f929766d21e57847c2475d7
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-07-09 13:18:09 +0530

    Time: 553 ms (24.73%), Space: 17.3 MB (26.17%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(vector<int>& nums, int index) {
-        if(index >= nums.size()) return 1e9;
-        if(index == nums.size()-1) {
-            return 0;
-        }
-        if(dp[index] != -1) return dp[index];
-        int ans = 1e9;
-        // try jumping 1 to nums[index]
-        for(int i=1;i<=nums[index];i++) {
-            ans = min(ans, 1+ solve(nums, index+i));
-        }
-        return dp[index] = ans;
-    }
-
     int jump(vector<int>& nums) {
-        dp.resize(nums.size(),-1);
-        return solve(nums, 0); 
+        vector<int> dp(nums.size(), 1e9);
+        dp[nums.size()-1] = 0;
+        for(int index = nums.size()-2;index>=0;index--) {
+            int ans = 1e9;
+            for(int i=1;i<=nums[index];i++) {
+                if(index+i >= nums.size()) continue;
+                ans = min(ans, 1+ dp[index+i]);
+            }
+            dp[index] = ans;
+        }
+        return dp[0]; 
     }
 };
