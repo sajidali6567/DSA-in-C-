2bf16bd3af6d693d6e6687f00e95e5f2046d731d
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-07-09 21:54:23 +0530

    Time: 341 ms (83.52%), Space: 136.9 MB (38.27%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    #define pp pair<int, int>
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> res;
-        deque<pp> q;
+        deque<pair<int, int>> q;
         for(int i=0;i<nums.size();i++) {
-            // if any values in front of array are out of window, pop them
+            // maintain a monotonically decreasing queue
             while(!q.empty() && q.front().second <= i-k) {
                 q.pop_front();
             }
-            // remove all the element whose valus are less than current value
             while(!q.empty() && q.back().first < nums[i]) {
                 q.pop_back();
             }
             q.push_back({nums[i], i});
-            // queue will always be sorted in decreasing order
-            if(i>=k-1)
+            if( i>= k-1) {
                 res.push_back(q.front().first);
+            }
         }
         return res;
     }
