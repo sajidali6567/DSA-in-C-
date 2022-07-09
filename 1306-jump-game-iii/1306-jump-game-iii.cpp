f4476042181b3013ef93a44b9a6b0b0dd8e65411
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-07-09 13:57:36 +0530

    Time: 91 ms (25.53%), Space: 34.6 MB (50.97%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    map<int, bool> mp;
+    vector<bool> visited;
     bool solve(vector<int>& arr, int start) {
-        mp[start] = true;
+        visited[start] = true;
         if(arr[start] == 0) return true;
         // we have two options, either go left or go right
         bool left = false, right = false;
-        if(start - arr[start] >=0 && mp.find(start - arr[start]) == mp.end())
+        if(start - arr[start] >=0 && !visited[start - arr[start]])
             left = canReach(arr, start - arr[start]);
-        if(start + arr[start] < arr.size() && mp.find(start + arr[start]) == mp.end())
+        if(start + arr[start] < arr.size() && !visited[start + arr[start]])
             right = canReach(arr, start + arr[start]);
         return left || right;       
     }
     bool canReach(vector<int>& arr, int start) {
+        visited.resize(arr.size(), false);
         return solve(arr, start);
     }
 };
