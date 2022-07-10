e4fcd592520be5093aa22db5453a1c01c1a9ded4
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-07-10 14:46:27 +0530

    Time: 297 ms (55.86%), Space: 81.4 MB (66.45%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> score(n);
-        score[0] = nums[0];
-        deque<int> dq;
-        dq.push_back(0);
-        for (int i = 1; i < n; i++) {
-            // pop the old index
-            while (!dq.empty() && dq.front() < i - k) {
-                dq.pop_front();
+        vector<int> dp(nums.size(), 0);   
+        dp[0] = nums[0];
+        deque<pair<int, int>> q;
+        q.push_back({nums[0], 0});
+        for(int i=1;i<nums.size();i++) {
+            // pop from the front of the queue which are out of range
+            while(!q.empty() && q.front().second < i-k) {
+                q.pop_front();
             }
-            score[i] = score[dq.front()] + nums[i];
-            // pop the smaller value
-            while (!dq.empty() && score[i] >= score[dq.back()]) {
-                dq.pop_back();
+    
+            dp[i] = nums[i] + q.front().first;
+            // pop from back of the queue which is less than dp[i]
+            while(!q.empty() && q.back().first <= dp[i]) {
+                q.pop_back();
             }
-            dq.push_back(i);
+            // push dp[i] window
+            q.push_back({dp[i], i});
         }
-        return score[n - 1];
+        return dp[nums.size()-1];
     }
 };
