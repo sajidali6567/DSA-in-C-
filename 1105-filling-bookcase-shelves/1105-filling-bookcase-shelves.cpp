cf6a74b827f5a9dc0a42ad89ddb902f02495e2da
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-08-13 23:30:50 +0530

    Time: 18 ms (18.32%), Space: 8.3 MB (39.39%) - LeetHub

---
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> dp;
+    int solve(vector<vector<int>>& books, int shelfWidth, int index) {
+        if(index == books.size()) {
+            return 0;
+        }
+        if(dp[index] != -1) return dp[index];
+        int curWidth = 0;
+        int maxHeight = INT_MIN;
+        int ans = INT_MAX;
+        for(int i=index;i<books.size();i++) {
+            curWidth += books[i][0];
+            int height = books[i][1];
+            if(curWidth <= shelfWidth) {
+                maxHeight = max(maxHeight, height);
+                ans = min(ans, maxHeight + solve(books, shelfWidth, i+1));
+            } else {
+                break;
+            }
+        }
+        return dp[index] = ans;
+    }
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        dp.resize(books.size(), -1);
+        return solve(books, shelfWidth, 0);
+    }
+};
