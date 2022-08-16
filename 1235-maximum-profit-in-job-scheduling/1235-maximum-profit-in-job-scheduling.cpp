60b522c21aa3a86dd8f19fe96087c1302716b7ce
Sajid Ali <50805850+sajidali6567@users.noreply.github.com>
2022-08-16 22:32:08 +0530

    Time: 241 ms (60.23%), Space: 64.4 MB (29.82%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> jobs;
-    vector<int> dp;
-    int solve(int index, vector<int>& startTime) {
-        if(index == jobs.size()) return 0;
-        if(dp[index] != -1) return dp[index];
+    int findMaxProfit(vector<vector<int>>& jobs) {
+        //  priority queue will store {end,profit}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        int maxProfit = 0;
+        for(auto job:jobs) {
+            int start = job[0], end = job[1], profitt = job[2];
+            // remove all the jobs from the queue whose end time is less than or equal to current job's start time
+            // but maxProfit should be stored for the chains we are throwing away
+            // concept: the chains we are throwing away is not conflicting with job i then it will not conflict with the any other      jobs after i, we do not need these chains anymore if we keep the maxProfit we are gaining from these chains
+            while(!pq.empty() && pq.top().first <= start) {
+                maxProfit = max(maxProfit, pq.top().second);
+                pq.pop();
+            }
+            // add current jobs into the most profitable chain and push it into the queue
+            pq.push({end, profitt+maxProfit});
+        }
         
-        int notPick = solve(index+1, startTime);
-        int nextIndex = lower_bound(startTime.begin(), startTime.end(), jobs[index][1]) - startTime.begin();
-        int pick = jobs[index][2] + solve(nextIndex, startTime);
-
-        return dp[index] = max(pick, notPick);
+        // check the queue for the maxProfit
+        while(!pq.empty()) {
+            maxProfit = max(maxProfit, pq.top().second);
+            pq.pop();
+        }
+        return maxProfit;       
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        dp.resize(profit.size(), -1);
+        vector<vector<int>> jobs;
         for(int i=0;i<profit.size();i++) {
             jobs.push_back({startTime[i], endTime[i], profit[i]});
         }
-        sort(jobs.begin(), jobs.end()); // sorting jobs according to their start time, so it will be easy to find next job 
-        for(int i=0;i<startTime.size();i++) {
-            startTime[i] = jobs[i][0];  // since we want to search in startTime, we need to store it seperately
-        }
-        return solve(0, startTime);
+        sort(jobs.begin(), jobs.end());
+        return findMaxProfit(jobs);
     }
 };
